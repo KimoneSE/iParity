def6ff4600ea280a490e5fe9636ea7ad7f42509a
Pangyk <1403842152@qq.com>
2017-04-14 12:41:52 +0800

    modify product
    修改product，删除一些类

--- src/com/ip/model/Product.java
+++ src/com/ip/model/Product.java
@@ -1,57 +1,130 @@
 package com.ip.model;
 
-import java.util.List;
-import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-/**
- * Created by windkl on 2017/4/11.
- */
+@Entity
+@Table(name="product")
 public class Product {
+	@Id
+	private String id;
+	// 商品名称
+	private String product;
+	// 分类名称
+	private String typeName;
+	// 是一个商店还是一个概括
+	private String parent;
+	// 库存(抓不到)
+	private int storage;
+	// 商店名
+	private String store;
+	// 商店链接
+	private String storeLink;
+	// 价格
+	private String price;
+	// 图片链接(只有概括有)
+	private String imageLink;
+	// 成交量(抓不到)
+	private int volumn;
+	// 描述(只有概括有)
+	private String description;
+	// 客户评分
+	private String value;
 
-    Set<Ad> ads;
-    List<Comment> comments;
-    Set<Link> links;
-    double price;
+	public String getId() {
+		return id;
+	}
 
-    public Product() {
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    public Product(Set<Ad> ads, List<Comment> comments, Set<Link> links, double price) {
-        this.ads = ads;
-        this.comments = comments;
-        this.links = links;
-        this.price = price;
-    }
+	public String getParent() {
+		return parent;
+	}
 
-    public Set<Ad> getAds() {
-        return ads;
-    }
+	public void setParent(String parent) {
+		this.parent = parent;
+	}
 
-    public void setAds(Set<Ad> ads) {
-        this.ads = ads;
-    }
+	public String getTypeName() {
+		return typeName;
+	}
 
-    public List<Comment> getComments() {
-        return comments;
-    }
+	public void setTypeName(String typeName) {
+		this.typeName = typeName;
+	}
 
-    public void setComments(List<Comment> comments) {
-        this.comments = comments;
-    }
+	public String getValue() {
+		return value;
+	}
 
-    public Set<Link> getLinks() {
-        return links;
-    }
+	public void setValue(String value) {
+		this.value = value;
+	}
 
-    public void setLinks(Set<Link> links) {
-        this.links = links;
-    }
+	public String getDescription() {
+		return description;
+	}
 
-    public double getPrice() {
-        return price;
-    }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-    public void setPrice(double price) {
-        this.price = price;
-    }
+	public String getProduct() {
+		return product;
+	}
+
+	public void setProduct(String product) {
+		this.product = product;
+	}
+
+	public int getStorage() {
+		return storage;
+	}
+
+	public void setStorage(int storage) {
+		this.storage = storage;
+	}
+
+	public String getStore() {
+		return store;
+	}
+
+	public void setStore(String store) {
+		this.store = store;
+	}
+
+	public String getStoreLink() {
+		return storeLink;
+	}
+
+	public void setStoreLink(String storeLink) {
+		this.storeLink = storeLink;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	public String getImageLink() {
+		return imageLink;
+	}
+
+	public void setImageLink(String imageLink) {
+		this.imageLink = imageLink;
+	}
+
+	public int getVolumn() {
+		return volumn;
+	}
+
+	public void setVolumn(int volumn) {
+		this.volumn = volumn;
+	}
 }
